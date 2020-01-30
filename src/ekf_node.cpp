eae01a759b5931e87851c890c99f919ac520ca83
Ryan O'Shea <rpo1202@gmail.com>
2020-01-29 21:06:34 -0500

    Skeleton code for the main structure of the code. Includes ROS setup and callbacks for the 3 main sensors.

--- src/ekf_node.cpp
+++ src/ekf_node.cpp
@@ -1,1 +1,99 @@
 // Extended Kalman Filter Node for BlueROV
+
+#include <iostream>
+#include "Eigen/Dense"
+#include "ros/ros.h"
+
+// IMU messages
+#include <sensor_msgs/Imu.h>
+
+// Depth sensor messages
+#include <bar30_depth/Depth.h>
+
+// DVL messages
+#include <rti_dvl/DVL.h>
+
+
+using std::cout;
+using std::endl;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+class EKF {
+private:
+	//State vector [x, y, roll, pitch, yaw, x_dot, y_dot, roll_dot, pitch_dot, yaw_dot] 
+	VectorXd state_;
+	
+	// State covariance matrix
+	MatrixXd cov_;
+
+	// state transition matrix
+	Eigen::MatrixXd F_;
+
+	// process covariance matrix
+	Eigen::MatrixXd Q_;
+
+	// measurement matrix
+	Eigen::MatrixXd H_;
+
+	// measurement covariance matrix
+	Eigen::MatrixXd R_;
+
+
+public:
+	// Constructor 
+	EKF(){};
+
+	// Destuctor
+	~EKF(){};
+
+	// Callback function for IMU messages from the VN 100 IMU
+	void IMUCallback(const sensor_msgs::Imu imu_msg)
+	{
+		cout << "IMU message:" << endl;
+		// cout << imu_msg << endl;
+	}
+
+	// Callback function for the depth messages from the bar30 Depth sensor
+	void depthCallback(const bar30_depth::Depth depth_msg)
+	{
+		cout << "Depth message:" << endl;
+		cout << depth_msg << endl;
+	}
+
+	// Callback function for the doppler velocity logger (DVL) messages from the **** DVL
+	void DVLCallback(const rti_dvl::DVL dvl_msg)
+	{
+		cout << "DVL message:" << endl;
+		cout << dvl_msg << endl;
+	}
+
+};
+
+
+// Main function that sets up the subscriber to each of the topics
+int main(int argc, char **argv)
+{
+	// Initialize the ROS node
+	ros::init(argc, argv, "EKF");
+
+	// Create the node handle to communicate with ROS
+	ros::NodeHandle n;
+
+	// Create and instance of the extended Kalman Filter Object
+	EKF ekf;
+
+	// Callback function for the IMU messages
+	ros::Subscriber imu_sub = n.subscribe("/vn100/imu/raw", 100, &EKF::IMUCallback, &ekf);
+
+	// Callback function for the depth messages
+	ros::Subscriber depth_sub = n.subscribe("/bar30/depth/raw", 100, &EKF::depthCallback, &ekf);
+
+	// Callback function for the DVL messages
+	ros::Subscriber dvl_sub = n.subscribe("/rti/body_velocity/raw", 100, &EKF::DVLCallback, &ekf);
+
+	ros::spin();
+
+	return 0;
+}
+
